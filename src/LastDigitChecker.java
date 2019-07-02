9dc706415d21509afc7f3473334c1c6e8530a283
Barzov-Pavel <barzov_pavel@mail.ru>
2019-07-02 10:07:35 +0300

    Упражнение по написанию кода 19: Last Digit Checker

---
+++ src/LastDigitChecker.java
@@ -0,0 +1,29 @@
+public class LastDigitChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+    }
+
+    public static boolean hasSameLastDigit(int number1, int number2, int number3) {
+        if (!isValid(number1) || !isValid(number2) || !isValid(number3)) {
+            return false;
+        }
+
+        int n1 = number1 % 10;
+        int n2 = number2 % 10;
+        int n3 = number3 % 10;
+
+        if (n1 == n2 || n1 == n3 || n2 == n3) {
+            return true;
+        } else return false;
+    }
+
+    public static boolean isValid(int number) {
+        if (number < 10 || number > 1000) {
+            return false;
+        }
+        return true;
+    }
+}
